8c319d6ca66e4676e79f21019e25fec82274e71e
seatter18 <64646854+seatter18@users.noreply.github.com>
2023-06-13 13:41:04 +0100

    Add files via upload

---
+++ ConstructXpress/ConstructXpress/Main.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace ConstructXpress
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_side = new System.Windows.Forms.Panel();
+            this.btn_staffPortal = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_accountManagement = new System.Windows.Forms.Button();
+            this.btn_shop = new System.Windows.Forms.Button();
+            this.btn_home = new System.Windows.Forms.Button();
+            this.pnl_menu_background = new System.Windows.Forms.Panel();
+            this.pnl_logo_outside = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pnl_top = new System.Windows.Forms.Panel();
+            this.tab_content = new System.Windows.Forms.TabControl();
+            this.page_home = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnl_side.SuspendLayout();
+            this.pnl_logo_outside.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tab_content.SuspendLayout();
+            this.page_home.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_side
+            // 
+            this.pnl_side.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.pnl_side.Controls.Add(this.btn_staffPortal);
+            this.pnl_side.Controls.Add(this.button3);
+            this.pnl_side.Controls.Add(this.btn_accountManagement);
+            this.pnl_side.Controls.Add(this.btn_shop);
+            this.pnl_side.Controls.Add(this.btn_home);
+            this.pnl_side.Controls.Add(this.pnl_menu_background);
+            this.pnl_side.Controls.Add(this.pnl_logo_outside);
+            this.pnl_side.Location = new System.Drawing.Point(0, -2);
+            this.pnl_side.Name = "pnl_side";
+            this.pnl_side.Size = new System.Drawing.Size(250, 725);
+            this.pnl_side.TabIndex = 0;
+            // 
+            // btn_staffPortal
+            // 
+            this.btn_staffPortal.FlatAppearance.BorderSize = 0;
+            this.btn_staffPortal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_staffPortal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_staffPortal.ForeColor = System.Drawing.Color.White;
+            this.btn_staffPortal.Location = new System.Drawing.Point(0, 345);
+            this.btn_staffPortal.Name = "btn_staffPortal";
+            this.btn_staffPortal.Size = new System.Drawing.Size(250, 59);
+            this.btn_staffPortal.TabIndex = 6;
+            this.btn_staffPortal.Text = "Staff Portal";
+            this.btn_staffPortal.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(0, 467);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 59);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Reports and Analytics";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btn_accountManagement
+            // 
+            this.btn_accountManagement.FlatAppearance.BorderSize = 0;
+            this.btn_accountManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_accountManagement.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_accountManagement.ForeColor = System.Drawing.Color.White;
+            this.btn_accountManagement.Location = new System.Drawing.Point(0, 406);
+            this.btn_accountManagement.Name = "btn_accountManagement";
+            this.btn_accountManagement.Size = new System.Drawing.Size(250, 59);
+            this.btn_accountManagement.TabIndex = 4;
+            this.btn_accountManagement.Text = "Account Management";
+            this.btn_accountManagement.UseVisualStyleBackColor = true;
+            // 
+            // btn_shop
+            // 
+            this.btn_shop.FlatAppearance.BorderSize = 0;
+            this.btn_shop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_shop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_shop.ForeColor = System.Drawing.Color.White;
+            this.btn_shop.Location = new System.Drawing.Point(0, 284);
+            this.btn_shop.Name = "btn_shop";
+            this.btn_shop.Size = new System.Drawing.Size(250, 59);
+            this.btn_shop.TabIndex = 3;
+            this.btn_shop.Text = "Shop";
+            this.btn_shop.UseVisualStyleBackColor = true;
+            // 
+            // btn_home
+            // 
+            this.btn_home.FlatAppearance.BorderSize = 0;
+            this.btn_home.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_home.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_home.ForeColor = System.Drawing.Color.White;
+            this.btn_home.Location = new System.Drawing.Point(0, 223);
+            this.btn_home.Name = "btn_home";
+            this.btn_home.Size = new System.Drawing.Size(250, 59);
+            this.btn_home.TabIndex = 1;
+            this.btn_home.Text = "Home";
+            this.btn_home.UseVisualStyleBackColor = true;
+            // 
+            // pnl_menu_background
+            // 
+            this.pnl_menu_background.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
+            this.pnl_menu_background.Location = new System.Drawing.Point(35, 230);
+            this.pnl_menu_background.Name = "pnl_menu_background";
+            this.pnl_menu_background.Size = new System.Drawing.Size(180, 298);
+            this.pnl_menu_background.TabIndex = 2;
+            // 
+            // pnl_logo_outside
+            // 
+            this.pnl_logo_outside.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.pnl_logo_outside.Controls.Add(this.pictureBox2);
+            this.pnl_logo_outside.Location = new System.Drawing.Point(25, 25);
+            this.pnl_logo_outside.Name = "pnl_logo_outside";
+            this.pnl_logo_outside.Size = new System.Drawing.Size(200, 150);
+            this.pnl_logo_outside.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ConstructXpress.Properties.Resources.logo;
+            this.pictureBox2.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(180, 130);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pnl_top
+            // 
+            this.pnl_top.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.pnl_top.Location = new System.Drawing.Point(250, -2);
+            this.pnl_top.Name = "pnl_top";
+            this.pnl_top.Size = new System.Drawing.Size(1243, 63);
+            this.pnl_top.TabIndex = 1;
+            // 
+            // tab_content
+            // 
+            this.tab_content.Controls.Add(this.page_home);
+            this.tab_content.Location = new System.Drawing.Point(246, 39);
+            this.tab_content.Name = "tab_content";
+            this.tab_content.SelectedIndex = 0;
+            this.tab_content.Size = new System.Drawing.Size(1253, 680);
+            this.tab_content.TabIndex = 1;
+            // 
+            // page_home
+            // 
+            this.page_home.Controls.Add(this.panel1);
+            this.page_home.Controls.Add(this.pictureBox1);
+            this.page_home.Location = new System.Drawing.Point(4, 22);
+            this.page_home.Name = "page_home";
+            this.page_home.Padding = new System.Windows.Forms.Padding(3);
+            this.page_home.Size = new System.Drawing.Size(1245, 654);
+            this.page_home.TabIndex = 1;
+            this.page_home.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ConstructXpress.Properties.Resources.background_splash;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1235, 654);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.panel1.Controls.Add(this.btn_login);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(421, 160);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(383, 275);
+            this.panel1.TabIndex = 1;
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.btn_login.FlatAppearance.BorderSize = 0;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.ForeColor = System.Drawing.Color.White;
+            this.btn_login.Location = new System.Drawing.Point(241, 197);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(112, 54);
+            this.btn_login.TabIndex = 13;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.label3.Font = new System.Drawing.Font("Myanmar Text", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 43);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.label2.Font = new System.Drawing.Font("Myanmar Text", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 43);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Username:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.label1.Font = new System.Drawing.Font("Myanmar Text", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 56);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Login";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(163, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 35);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(163, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 35);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(1484, 711);
+            this.Controls.Add(this.pnl_side);
+            this.Controls.Add(this.tab_content);
+            this.Controls.Add(this.pnl_top);
+            this.MaximumSize = new System.Drawing.Size(1500, 750);
+            this.MinimumSize = new System.Drawing.Size(1500, 750);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.pnl_side.ResumeLayout(false);
+            this.pnl_logo_outside.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tab_content.ResumeLayout(false);
+            this.page_home.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_side;
+        private System.Windows.Forms.Panel pnl_top;
+        private System.Windows.Forms.TabControl tab_content;
+        private System.Windows.Forms.TabPage page_home;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnl_logo_outside;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn_home;
+        private System.Windows.Forms.Button btn_staffPortal;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_accountManagement;
+        private System.Windows.Forms.Button btn_shop;
+        private System.Windows.Forms.Panel pnl_menu_background;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
